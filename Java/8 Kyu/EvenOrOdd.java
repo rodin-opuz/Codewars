6a5b797d346aad0b9b753a7d09dc1dcfbadc60cd
Rodin Opuz <opuzrodin@gmail.com>
2025-09-07 16:45:14 +0100

    Create EvenOrOdd.java

---
+++ Java/8 Kyu/EvenOrOdd.java
@@ -0,0 +1,9 @@
+public class EvenOrOdd {
+    public static String evenOrOdd(int number) {
+      if (number % 2 == 0) {
+        return "Even";
+      } else {
+        return "Odd";
+      }
+    }
+}

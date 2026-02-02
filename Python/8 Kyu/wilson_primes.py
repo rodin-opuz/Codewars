e36ff723e884bb17430f3b1083259fa3df981e09
Rodin Opuz <opuzrodin@gmail.com>
2026-02-02 13:55:28 +0000

    Simplify am_i_wilson function for specific primes

--- Python/8 Kyu/wilson_primes.py
+++ Python/8 Kyu/wilson_primes.py
@@ -1,4 +1,7 @@
-import math
+def am_i_wilson(n):
+    return n in (5, 13, 563)
+
+'''import math
 def am_i_wilson(n):
     if n < 2: return False
-    return (math.factorial(n - 1) + 1) % (n * n) == 0
+    return (math.factorial(n - 1) + 1) % (n * n) == 0'''
